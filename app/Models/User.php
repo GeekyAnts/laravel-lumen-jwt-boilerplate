2e2bfe3179ccb7f0b9910a07d1fa013ea83b854d
Faiz A Farooqui <faizz.af@gmail.com>
2018-11-23 09:35:02 +0530

    fixes: JWT trait added to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use App\Traits\JWT as JWTTrait;
+
 class User extends Authenticatable
 {
-    use Notifiable;
+    use JWTTrait, Notifiable;
 
     /**
      * The attributes that are mass assignable.
---
+++ app/Traits/JWT.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Traits;
+
+trait JWT 
+{
+	/**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
